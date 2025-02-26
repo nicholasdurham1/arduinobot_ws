e436fa9e39bac0f552308b14b794e42e0c6a5293
nadurham <nadurham.nd@gmail.com>
2025-02-26 14:56:41 -0800

    Creating Launch Files

--- build/arduinobot_description/cmake_install.cmake
+++ build/arduinobot_description/cmake_install.cmake
@@ -46,6 +46,8 @@ if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/arduinobot_description" TYPE DIRECTORY FILES
     "/home/nadurham/arduinobot_ws/src/arduinobot_description/meshes"
     "/home/nadurham/arduinobot_ws/src/arduinobot_description/urdf"
+    "/home/nadurham/arduinobot_ws/src/arduinobot_description/launch"
+    "/home/nadurham/arduinobot_ws/src/arduinobot_description/rviz"
     )
 endif()
 
